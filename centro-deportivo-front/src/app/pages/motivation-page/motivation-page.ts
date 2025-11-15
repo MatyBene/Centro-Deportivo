22569851be0ae761c58f059874019fc0c6d455fe
TomasB <tomasbservant@gmail.com>
2025-11-15 16:20:14 -0300

    crea componente motivation-page y configura enrutamiento

---
+++ centro-deportivo-front/src/app/pages/motivation-page/motivation-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-motivation-page',
+  imports: [],
+  templateUrl: './motivation-page.html',
+  styleUrl: './motivation-page.css'
+})
+export class MotivationPage {
+
+}
